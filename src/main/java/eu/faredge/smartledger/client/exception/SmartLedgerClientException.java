be585a2e48869344910d97387783b9d6d4133fcc
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2017-10-06 15:18:14 +0200

    Inserted a project checked exception to use in the library.

--- src/main/java/eu/faredge/smartledger/client/exception/SmartLedgerClientException.java
+++ src/main/java/eu/faredge/smartledger/client/exception/SmartLedgerClientException.java
@@ -1,4 +1,7 @@
 package eu.faredge.smartledger.client.exception;
 
+//TODO Modify Test to use this exception
+// create some status codes to manage events coming from Fabric, but even possible event happening
+// like the absence of a Fabric instance at the moment.
 public class SmartLedgerClientException extends Exception {
 }
